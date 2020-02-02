b740245d65f967dc50f2f7de7517a75fb72b474e
jinyuttt <jinyu20072787@sina.com>
2020-02-03 01:57:53 +0800

    Signed-off-by: jinyuttt <jinyu20072787@sina.com>

--- FastMapper/obj/Debug/netstandard2.0/FastMapperEmit.AssemblyInfo.cs
+++ FastMapper/obj/Debug/netstandard2.0/FastMapperEmit.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastMapperEmit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastMapperEmit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastMapperEmit")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
